8a99b5c756d16ad82a307f282811ad0b98d04bfd
inoplanetyanya <inoplanetyanya97@gmail.com>
2025-03-11 22:57:33 +0300

    allow all origins

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -29,7 +29,7 @@ func (h *Handler) InitRoutes() http.Handler {
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://192.168.0.25:3000")
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
